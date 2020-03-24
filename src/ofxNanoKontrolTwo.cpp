0039d0daef6fa63713f1e691855a824e1e5b3792
Luca esnho Moroni <esnhoesnho@gmail.com>
2020-03-24 12:18:36 +0100

    Removed verbose logging

--- src/ofxNanoKontrolTwo.cpp
+++ src/ofxNanoKontrolTwo.cpp
@@ -9,8 +9,6 @@
 
 void ofxNanoKontrolTwo::setup() {
 
-    ofSetLogLevel(OF_LOG_VERBOSE);
-
     for (int i = 0; i < 8; i++) {
         controlsCallbacks controlCallbacksGroup;
         controlsValues controlValuesGroup;
